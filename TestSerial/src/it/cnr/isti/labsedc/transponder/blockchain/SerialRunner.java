fa4ec1ffa463f0ae9715b7d509ed3568a08f2aa2
acalabro <antonello.calabro@isti.cnr.it>
2024-06-08 18:35:13 +0200

    asd

---
+++ TestSerial/src/it/cnr/isti/labsedc/transponder/blockchain/SerialRunner.java
@@ -0,0 +1,31 @@
+package it.cnr.isti.labsedc.transponder.blockchain;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+public class SerialRunner {
+
+	public static void main(String[] args) {
+		SerialPort[] commPorts;
+		commPorts = SerialPort.getCommPorts();
+		
+		for (int i = 0; i<commPorts.length; i++) {
+			System.out.println(commPorts[i].getSystemPortName());
+		}
+	}
+	
+	public void ReadFromSerial(SerialPort comPort) {
+		comPort.openPort();
+		try {
+		   while (true)
+		   {
+		      while (comPort.bytesAvailable() == 0)
+		         Thread.sleep(20);
+
+		      byte[] readBuffer = new byte[comPort.bytesAvailable()];
+		      int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+		      System.out.println("Read " + numRead + " bytes.");
+		   }
+		} catch (Exception e) { e.printStackTrace(); }
+		comPort.closePort();	
+	}
+}
